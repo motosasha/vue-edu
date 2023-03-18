dcc30a055a2a00c2cc250ebcda8bc93cbbef134d
motosasha <39082380+motosasha@users.noreply.github.com>
2023-03-18 23:56:15 +0300

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import router from "@/router/router";
-import App from './App.vue'
-import './assets/main.css'
-
-createApp(App)
-	.use(router)
-	.mount('#app');
--- src/pages/Main.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div>I use routing!<br> Click on links above ↑</div>
-</template>
-
-<script>
-export default {
-	name: "Main",
-}
-</script>
